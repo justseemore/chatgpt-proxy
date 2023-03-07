f413830330b1e377ab40b5eb807edfbc12e333aa
zhaopengfei <hbyx.zpf@gmail.com>
2023-03-07 10:46:20 +0800

    fix

---
+++ api/index.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"fmt"
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"net/http"
+)
+
+// Handler is the main entry point of the application. Think of it like the main() method
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// This is needed to set the proper request path in `*fiber.Ctx`
+	r.RequestURI = r.URL.String()
+
+	handler().ServeHTTP(w, r)
+}
+
+// building the fiber application
+func handler() http.HandlerFunc {
+	app := fiber.New()
+	app.Get("/*", func(ctx *fiber.Ctx) error {
+		proxyUrl := fmt.Sprintf("https://api.openai.com/%s", ctx.Params("*", ""))
+		if err := proxy.Do(ctx, proxyUrl); err != nil {
+			return err
+		}
+		ctx.Response().Header.Del(fiber.HeaderServer)
+		return nil
+	})
+	return adaptor.FiberApp(app)
+}
