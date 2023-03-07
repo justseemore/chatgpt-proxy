d343187382cc0ecfb2675d6e5cfe72527709a77f
zhaopengfei <hbyx.zpf@gmail.com>
2023-03-07 11:10:47 +0800

    fix

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
+	"os"
 )
 
 func main() {
 	app := fiber.New()
 	app.All("/*", func(ctx *fiber.Ctx) error {
-		proxyUrl := fmt.Sprintf("https://api.openai.com/%s", ctx.Params("*", ""))
+		proxyUrl := fmt.Sprintf("https://%s/%s", os.Getenv("PROXY_DOMAIN"), ctx.Params("*", ""))
 		if err := proxy.Do(ctx, proxyUrl); err != nil {
 			return err
 		}
