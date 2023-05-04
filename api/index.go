67f807d1c5feb0c738ec5dfb99b72b86afb19d75
zhaopengfei <hbyx.zpf@gmail.com>
2023-05-04 14:25:26 +0800

    fix

--- api/index.go
+++ api/index.go
@@ -24,12 +24,14 @@ func handler() http.HandlerFunc {
 
 	app.All("/*", func(ctx *fiber.Ctx) error {
 		OsAuthApiKey := os.Getenv("AUTH_API_KEY")
+		proxyHost := os.Getenv("PROXY_DOMAIN")
 		if OsAuthApiKey != "" && ctx.Get("auth-api-key", "") == OsAuthApiKey {
 			ctx.Request().Header.Del("Authorization")
 			ctx.Request().Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("OPENAI_API_KEY")))
 			ctx.Request().Header.Del("auth-api-key")
+		} else {
+			proxyHost, _ = strings.CutSuffix(ctx.Get("h-proxy-host", os.Getenv("PROXY_DOMAIN")), "/")
 		}
-		proxyHost, _ := strings.CutSuffix(ctx.Get("h-proxy-host", os.Getenv("PROXY_DOMAIN")), "/")
 		path, _ := strings.CutPrefix(ctx.OriginalURL(), "/")
 		proxyUrl := fmt.Sprintf("https://%s/%s", proxyHost, path)
 		if err := proxy.Do(ctx, proxyUrl); err != nil {
